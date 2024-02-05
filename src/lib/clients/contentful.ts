78328cc249c4b65b79d58aca367ebe14af0a7078
Phillip Malboeuf <phil@phils.computer>
2024-02-05 14:24:01 -0500

    contentful 2.0

--- src/lib/clients/contentful.ts
+++ src/lib/clients/contentful.ts
@@ -3,6 +3,7 @@ import contentful from 'contentful'
 const preview = true
 export const content = contentful.createClient({
   space: '2g43ft9kwm8s',
+  environment: '2.0',
   host: preview ? 'preview.contentful.com' : 'cdn.contentful.com',
-  accessToken: preview ? 'bzeyoF0MTUOOHjBfa_SWxbWXJtQGcSHtLuxY8U9WHAI' : '7-3Uo3dqE-tm2XJOO3t_JJl9-06ZkJi3vRqqQHGNBeI',
+  accessToken: preview ? '9ncKYNaOEDCcxwbukziNiRI7WdFhk1-g057bTsNiS40' : 'rDDr4skd_pn69p2aeK4fBIgheqjD3m0Wmf3hUBebhK8',
 }).withoutUnresolvableLinks
